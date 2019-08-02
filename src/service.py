45b03bf3252f04f8805e33925bf51d9672a57cb2
IcerOut <44326661+IcerOut@users.noreply.github.com>
2019-08-02 14:49:01 +0300

    Fixed conversions, added console output

--- src/service.py
+++ src/service.py
@@ -13,30 +13,31 @@ from src.exceptions import InvalidColorError
 
 ConvertedColor = namedtuple('ConvertedColor', 'name hex rgb hsl')
 
-COLOR_NAMES = [['Black', 'Navy', 'DarkBlue', 'MediumBlue', 'Blue', 'DarkGreen', 'Green', 'DarkCyan',
-                'DeepSkyBlue', 'DarkTurquoise', 'MediumSpringGreen', 'Lime', 'SpringGreen', 'Cyan',
-                'MidnightBlue', 'DodgerBlue', 'LightSeaGreen', 'ForestGreen', 'SeaGreen',
-                'DarkSlateGray', 'LimeGreen', 'MediumSeaGreen', 'Turquoise', 'RoyalBlue',
-                'SteelBlue', 'DarkSlateBlue', 'MediumTurquoise', 'Indigo', 'DarkOliveGreen',
-                'CadetBlue', 'CornflowerBlue', 'MediumAquamarine', 'DimGray', 'SlateBlue',
-                'OliveDrab', 'SlateGray', 'LightSlateGray', 'MediumSlateBlue', 'LawnGreen',
-                'Chartreuse', 'Aquamarine', 'Maroon', 'Purple', 'Olive', 'Gray', 'LightSlateBlue',
-                'SkyBlue', 'LightSkyBlue', 'BlueViolet', 'DarkRed', 'DarkMagenta', 'SaddleBrown',
-                'DarkSeaGreen', 'LightGreen', 'MediumPurple', 'DarkViolet', 'PaleGreen',
-                'DarkOrchid', 'YellowGreen', 'Sienna', 'Brown', 'DarkGray', 'LightBlue',
-                'GreenYellow', 'PaleTurquoise', 'LightSteelBlue', 'PowderBlue', 'Firebrick',
-                'DarkGoldenrod', 'MediumOrchid', 'RosyBrown', 'DarkKhaki', 'Silver',
-                'MediumVioletRed', 'IndianRed', 'Peru', 'VioletRed', 'Chocolate', 'Tan',
-                'LightGray', 'Thistle', 'Orchid', 'Goldenrod', 'PaleVioletRed', 'Crimson',
-                'Gainsboro', 'Plum', 'Burlywood', 'LightCyan', 'Lavender', 'DarkSalmon', 'Violet',
-                'LightGoldenrod', 'PaleGoldenrod', 'LightCoral', 'Khaki', 'AliceBlue', 'Honeydew',
-                'Azure', 'SandyBrown', 'Wheat', 'Beige', 'WhiteSmoke', 'MintCream', 'GhostWhite',
-                'Salmon', 'AntiqueWhite', 'Linen', 'LightGoldenrodYellow', 'OldLace', 'Red',
-                'Magenta', 'DeepPink', 'OrangeRed', 'Tomato', 'HotPink', 'Coral', 'DarkOrange',
-                'LightSalmon', 'Orange', 'LightPink', 'Pink', 'Gold', 'PeachPuff', 'NavajoWhite',
-                'Moccasin', 'Bisque', 'MistyRose', 'BlanchedAlmond', 'PapayaWhip', 'LavenderBlush',
-                'Seashell', 'Cornsilk', 'LemonChiffon', 'FloralWhite', 'Snow', 'Yellow',
-                'LightYellow', 'Ivory', 'White']]
+# noinspection SpellCheckingInspection
+COLOR_NAMES = ('black', 'navy', 'darkblue', 'mediumblue', 'blue', 'darkgreen', 'green', 'darkcyan',
+               'deepskyblue', 'darkturquoise', 'mediumspringgreen', 'lime', 'springgreen', 'cyan',
+               'midnightblue', 'dodgerblue', 'lightseagreen', 'forestgreen', 'seagreen',
+               'darkslategray', 'limegreen', 'mediumseagreen', 'turquoise', 'royalblue',
+               'steelblue', 'darkslateblue', 'mediumturquoise', 'indigo', 'darkolivegreen',
+               'cadetblue', 'cornflowerblue', 'mediumaquamarine', 'dimgray', 'slateblue',
+               'olivedrab', 'slategray', 'lightslategray', 'mediumslateblue', 'lawngreen',
+               'chartreuse', 'aquamarine', 'maroon', 'purple', 'olive', 'gray', 'lightslateblue',
+               'skyblue', 'lightskyblue', 'blueviolet', 'darkred', 'darkmagenta', 'saddlebrown',
+               'darkseagreen', 'lightgreen', 'mediumpurple', 'darkviolet', 'palegreen',
+               'darkorchid', 'yellowgreen', 'sienna', 'brown', 'darkgray', 'lightblue',
+               'greenyellow', 'paleturquoise', 'lightsteelblue', 'powderblue', 'firebrick',
+               'darkgoldenrod', 'mediumorchid', 'rosybrown', 'darkkhaki', 'silver',
+               'mediumvioletred', 'indianred', 'peru', 'violetred', 'chocolate', 'tan', 'lightgray',
+               'thistle', 'orchid', 'goldenrod', 'palevioletred', 'crimson', 'gainsboro', 'plum',
+               'burlywood', 'lightcyan', 'lavender', 'darksalmon', 'violet', 'lightgoldenrod',
+               'palegoldenrod', 'lightcoral', 'khaki', 'aliceblue', 'honeydew', 'azure',
+               'sandybrown', 'wheat', 'beige', 'whitesmoke', 'mintcream', 'ghostwhite', 'salmon',
+               'antiquewhite', 'linen', 'lightgoldenrodyellow', 'oldlace', 'red', 'magenta',
+               'deeppink', 'orangered', 'tomato', 'hotpink', 'coral', 'darkorange', 'lightsalmon',
+               'orange', 'lightpink', 'pink', 'gold', 'peachpuff', 'navajowhite', 'moccasin',
+               'bisque', 'mistyrose', 'blanchedalmond', 'papayawhip', 'lavenderblush', 'seashell',
+               'cornsilk', 'lemonchiffon', 'floralwhite', 'snow', 'yellow', 'lightyellow', 'ivory',
+               'white')
 
 
 def _replace_separators(string: str) -> str:
@@ -45,10 +46,17 @@ def _replace_separators(string: str) -> str:
     :param string: The input string containing the separators
     :return: A string with the separators replaced by '*'
     """
-    string.replace(', ', '*')
-    string.replace(',', '*')
-    string.replace(' ', '*')
-    return string
+    return string.replace(', ', '*').replace(',', '*').replace(' ', '*')
+
+
+def _format_name(name: str) -> str:
+    """
+    Formats the color name to start with capital letters and have spaces where necessary
+    :param name: The original name color
+    :return: The formatted name color
+    """
+    name = re.sub(r"(\w)([A-Z])", r"\1 \2", name)
+    return name.title()
 
 
 def _transform_percentage(string: str) -> float:
@@ -74,7 +82,7 @@ def _transform_percentage(string: str) -> float:
     return val
 
 
-def identify_and_create_object(user_input: str) -> Color:
+def _identify_and_create_object(user_input: str) -> Color:
     """
     Identifies the data type of an input, converts it appropriately and creates the Color() object
     :param user_input: The input string from the input textbox
@@ -84,20 +92,24 @@ def identify_and_create_object(user_input: str) -> Color:
     result = Color()
 
     # Checks for literal color names
-    if user_input in COLOR_NAMES:
-        result = Color(user_input)
+    tmp = re.sub(r"[^a-zA-Z]+", "", user_input.lower())
+    if tmp in COLOR_NAMES:
+        result = Color(tmp)
+        print(f'\nDetected literal: {tmp}')
         return result
 
     # Matches a hex color without the '#' sign
     hex_without_pound_sign_pattern = re.compile(r'[A-Fa-f0-9]{6}')
     if hex_without_pound_sign_pattern.match(user_input) and len(user_input) == 6:
-        result.hex_l = '#' + user_input.lower()
+        result.set_hex_l('#' + user_input.lower())
+        print("\nDetected hex without '#'...")
         return result
 
     # Matches a hex color with the '#' sign
     hex_with_pound_sign_pattern = re.compile(r'#[A-Fa-f0-9]{6}')
     if hex_with_pound_sign_pattern.match(user_input) and len(user_input) == 7:
-        result.hex_l = user_input.lower()
+        result.set_hex_l(user_input.lower())
+        print("\nDetected hex with '#'...")
         return result
 
     # Matches an RGB color (3x 1-3 digits numbers, separated by up to 2 of ',' and ' ')
@@ -109,7 +121,8 @@ def identify_and_create_object(user_input: str) -> Color:
         if not 0 <= r_value <= 255 or not 0 <= g_value <= 255 or not 0 <= b_value <= 255:
             raise InvalidColorError(
                     'This looks like an RGB color but the values are not in the 0-255 range!')
-        result.rgb = (r_value / 255, g_value / 255, b_value / 255)
+        result.set_rgb((r_value / 255, g_value / 255, b_value / 255))
+        print(f"\nDetected rgb: {user_input}")
         return result
 
     # Matches a HSL color (1x 1-3 digits number, then 2x percentages or numbers in the range 0-1)
@@ -117,7 +130,6 @@ def identify_and_create_object(user_input: str) -> Color:
     if hsl_pattern.match(user_input):
         user_input = _replace_separators(user_input)
         h_value, s_value, l_value = user_input.split('*')
-
         h_value = int(h_value)
         if not 0 <= h_value <= 360:
             raise InvalidColorError(
@@ -134,16 +146,31 @@ def identify_and_create_object(user_input: str) -> Color:
             raise InvalidColorError(
                     'This looks like a HSL color but the Saturation or Lightness values are not '
                     'percentages or in the 0-1 range!')
-        result.hsl = (h_value, s_value, l_value)
+        print(h_value, s_value, l_value)
+        result.set_hsl((h_value / 360, s_value, l_value))
+        print(f"\nDetected hsl: {user_input}")
         return result
 
     raise InvalidColorError("The input doesn't fit any of the known color patterns!")
 
 
-def convert_color(color: Color) -> ConvertedColor:
+def convert(user_input: str) -> ConvertedColor:
     """
-    Takes a Color object and converts it into a ConvertedColor namedtuple
-    :param color: The color object
+    Takes a the user input and converts it into a ConvertedColor namedtuple
+    :param user_input: The input string from the input textbox
     :return: A ConvertedColor namedtuple
     """
-    pass
+
+    # Strips left and right spacing from the user input
+    user_input = user_input.lstrip().rstrip()
+
+    color = _identify_and_create_object(user_input)
+
+    name_value = _format_name(color.get_web()) if not color.get_web().startswith('#') else None
+    hex_value = color.get_hex_l().upper()
+    rgb_value = ', '.join([str(int(nr * 255)) for nr in color.get_rgb()])
+    hsl_value = ', '.join([str(int(color.get_hsl()[0] * 360)),
+                           str(int(round(color.get_hsl()[1], 2) * 100)) + '%',
+                           str(int(round(color.get_hsl()[2], 2) * 100)) + '%'])
+
+    return ConvertedColor(name_value, hex_value, rgb_value, hsl_value)
