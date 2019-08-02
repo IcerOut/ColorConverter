67c2e0e125bbf8f7feffe1be45e45110ea6015e8
IcerOut <44326661+IcerOut@users.noreply.github.com>
2019-08-02 14:49:19 +0300

    Improved GUI

--- src/globals.py
+++ src/globals.py
@@ -6,3 +6,11 @@ ColorConverter
 """
 
 APP_TITLE = 'Color Converter v0.1'
+
+DISCORD_DARK = '#2c2f33'
+
+DISCORD_DARK_HOVER = '#23272a'
+
+DISCORD_LIGHT = '#99aab5'
+
+DISCORD_TEXTBOX = '#323539'
--- src/gui.py
+++ src/gui.py
@@ -4,10 +4,11 @@ ColorConverter
 @author: Lung Alin-Sebastian
 
 """
-import colour
 import tkinter as tk
 
-from src.globals import APP_TITLE
+from src.exceptions import InvalidColorError
+from src.globals import APP_TITLE, DISCORD_DARK, DISCORD_LIGHT, DISCORD_TEXTBOX
+from src.service import convert
 
 
 class GUI:
@@ -18,36 +19,90 @@ class GUI:
     def __init__(self, master):
         self.master = master
         self.master.title(APP_TITLE)
+        self.master.configure(bg=DISCORD_DARK)
+
+        #
+        # Minimum size config
+        for x in range(5):
+            self.master.grid_rowconfigure(x, minsize=70, weight=1)
+
+        self.master.grid_columnconfigure(0, minsize=50, weight=1)
+        self.master.grid_columnconfigure(1, minsize=150, weight=1)
 
         #
         # Input TextBox
         self.input_value = tk.StringVar(self.master)
-        self.input_textbox = tk.Entry(self.master, textvariable=self.input_value)
-        self.input_textbox.grid(row=0, column=0, columnspan=3)
+        self.input_value.set('')
+        self.input_textbox = tk.Entry(self.master, textvariable=self.input_value,
+                                      font=('Verdana', 36), width=15, fg=DISCORD_LIGHT,
+                                      bg=DISCORD_TEXTBOX, justify='center')
+        self.input_textbox.grid(row=0, column=0, columnspan=2)
+        self.old_input = ''
+
+        #
+        # Name output
+        self.name_label = tk.Label(self.master, text='Name: ', font=('Verdana', 24),
+                                   fg=DISCORD_LIGHT, bg=DISCORD_DARK)
+        self.name_label.grid(row=1, column=0)
+        self.name_value = tk.StringVar(self.master)
+        self.name_textbox = tk.Entry(self.master, textvariable=self.name_value,
+                                     font=('Verdana', 24), width=15, fg=DISCORD_LIGHT,
+                                     bg=DISCORD_TEXTBOX, justify='center')
+        self.name_textbox.grid(row=1, column=1)
 
         #
         # Hex output
-        self.hex_label = tk.Label(self.master, text="Hex: ")
-        self.hex_label.grid(row=1, column=0)
+        self.hex_label = tk.Label(self.master, text='Hex: ', font=('Verdana', 24),
+                                  fg=DISCORD_LIGHT, bg=DISCORD_DARK)
+        self.hex_label.grid(row=2, column=0)
         self.hex_value = tk.StringVar(self.master)
-        self.hex_textbox = tk.Entry(self.master, textvariable=self.hex_value)
-        self.hex_textbox.grid(row=1, column=1)
+        self.hex_textbox = tk.Entry(self.master, textvariable=self.hex_value,
+                                    font=('Verdana', 24), width=15, fg=DISCORD_LIGHT,
+                                    bg=DISCORD_TEXTBOX, justify='center')
+        self.hex_textbox.grid(row=2, column=1)
 
         #
-        # RGB/RGBA output
-        self.rgb_label = tk.Label(self.master, text="RGB/RGBA: ")
-        self.rgb_label.grid(row=2, column=0)
+        # RGB output
+        self.rgb_label = tk.Label(self.master, text='RGB: ', font=('Verdana', 24),
+                                  fg=DISCORD_LIGHT, bg=DISCORD_DARK)
+        self.rgb_label.grid(row=3, column=0)
         self.rgb_value = tk.StringVar(self.master)
-        self.rgb_textbox = tk.Entry(self.master, textvariable=self.rgb_value)
-        self.rgb_textbox.grid(row=2, column=1)
+        self.rgb_textbox = tk.Entry(self.master, textvariable=self.rgb_value,
+                                    font=('Verdana', 24), width=15, fg=DISCORD_LIGHT,
+                                    bg=DISCORD_TEXTBOX, justify='center')
+        self.rgb_textbox.grid(row=3, column=1)
 
         #
-        # HSL/HSLA output
-        self.hsl_label = tk.Label(self.master, text="HSL/HSLA: ")
-        self.hsl_label.grid(row=3, column=0)
+        # HSL output
+        self.hsl_label = tk.Label(self.master, text='HSL: ', font=('Verdana', 24),
+                                  fg=DISCORD_LIGHT, bg=DISCORD_DARK)
+        self.hsl_label.grid(row=4, column=0)
         self.hsl_value = tk.StringVar(self.master)
-        self.hsl_textbox = tk.Entry(self.master, textvariable=self.hsl_value)
-        self.hsl_textbox.grid(row=3, column=1)
+        self.hsl_textbox = tk.Entry(self.master, textvariable=self.hsl_value,
+                                    font=('Verdana', 24), width=15, fg=DISCORD_LIGHT,
+                                    bg=DISCORD_TEXTBOX, justify='center')
+        self.hsl_textbox.grid(row=4, column=1)
 
-    def continuous_convert(self):
-        pass
+        # Initial call of the function. Afterwards it will keep calling itself every 0.5 seconds
+        self.continuous_convert()
+
+    def continuous_convert(self) -> None:
+        """
+        Continuously monitors the input textbox and dropdown for changes (every 0.5 seconds)
+        When it detects a change, it updates the conversions
+        """
+        user_input = self.input_value.get()
+        if user_input != self.old_input:
+            self.old_input = user_input
+            print('Change detected, attempting conversion...', end='')
+            try:
+                new_color = convert(user_input)
+                self.name_value.set(new_color.name)
+                self.hex_value.set(new_color.hex)
+                self.rgb_value.set(new_color.rgb)
+                self.hsl_value.set(new_color.hsl)
+            except ValueError:
+                print(' Invalid number or decimal entered.')
+            except InvalidColorError:
+                print(' No format recognized.')
+        self.master.after(500, self.continuous_convert)
